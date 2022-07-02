cae4d7281ee47aae519a7900463e5b05babe0e9a
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2022-07-02 17:46:22 +0200

    Added Items

---
+++ Lorenzo-Todisco/AbstractItem.cs
@@ -0,0 +1,53 @@
+﻿namespace OOP_Project
+{
+    abstract class AbstractItem
+    {
+        private Position _pos;
+        public string Name { get; set; }
+        public Position Position { get => _pos; }
+
+        public AbstractItem(string name, Position position)
+        {
+            Name = name;
+            _pos = position;
+        }
+
+        public AbstractItem(string name, int x, int y)
+        {
+            Name = name;
+            _pos = new Position(x, y);
+        }
+
+        public void setPos(Position p)
+        {
+            _pos.SetPosition(p);
+        }
+
+        public void setPos(int x, int y)
+        {
+            _pos.SetPosition(x, y);
+        }
+        public override bool Equals(object? obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                AbstractItem i = (AbstractItem)obj;
+                return (Name == i.Name) && (Position == i.Position);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"\n{Name} [{Position}]";
+        }
+    }
+}
