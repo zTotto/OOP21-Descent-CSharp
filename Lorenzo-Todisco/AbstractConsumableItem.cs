4146d70e9a7eec0d971e5623ec47fb6d7b79cfd5
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2022-07-03 12:28:03 +0200

    Added consumable and potion classes

---
+++ Lorenzo-Todisco/AbstractConsumableItem.cs
@@ -0,0 +1,27 @@
+﻿namespace LorenzoTodisco
+{
+    abstract class AbstractConsumableItem : AbstractItem
+    {
+        public float Modifier { get; }
+        public AbstractConsumableItem(string name, Position position, float modifier) : base(name, position)
+        {
+            Modifier = modifier;
+        }
+
+        public abstract void Use(Character pg);
+        public abstract bool CanUse(Character pg);
+
+        public override bool Equals(object? obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                AbstractConsumableItem i = (AbstractConsumableItem)obj;
+                return (Name == i.Name) && (Modifier == i.Modifier);
+            }
+        }
+    }
+}
---
+++ Lorenzo-Todisco/HealthPotion.cs
@@ -0,0 +1,17 @@
+﻿namespace LorenzoTodisco
+{
+    class HealthPotion : AbstractConsumableItem
+    {
+        public HealthPotion(string name, Position position, float modifier) : base(name, position, modifier) { }
+
+        public override bool CanUse(Character pg)
+        {
+            return pg.CurrentHp < pg.MaxHp;
+        }
+
+        public override void Use(Character pg)
+        {
+            pg.CurrentHp = pg.CurrentHp + (int)(pg.MaxHp * Modifier);
+        }
+    }
+}
