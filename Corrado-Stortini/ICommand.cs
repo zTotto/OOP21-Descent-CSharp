e737e196aa542752684bed594d039b0065419882
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2022-07-07 12:34:32 +0200

    Documentation of ICommand.cs

--- Corrado-Stortini/ICommand.cs
+++ Corrado-Stortini/ICommand.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace CorradoStortini
 {
+    /// <summary>
+    /// Functional interface of a Command.
+    /// </summary>
     public interface ICommand
     {
+        /// <summary>
+        /// Execute a command on a specific character.
+        /// </summary>
+        /// <param name="character">the character on which the command will be executed</param>
         void ExecuteCommand(Character character);
     }
 }
