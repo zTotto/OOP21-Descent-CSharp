1642e3bc9f29042c42f72d1e3d994fd02a82040a
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2022-07-07 12:40:07 +0200

    Documentation of Movement.cs

--- Corrado-Stortini/Movement.cs
+++ Corrado-Stortini/Movement.cs
@@ -4,15 +4,23 @@ using System.Text;
 
 namespace CorradoStortini
 {
+    /// <summary>
+    /// Class used to move a character based on the specified direction
+    /// </summary>
     public class Movement : ICommand
     {
         private readonly Direction _direction;
 
+        /// <summary>
+        /// Constructor for the Movement class
+        /// </summary>
+        /// <param name="direction">Direction that a character must follow when moving through this class</param>
         public Movement(Direction direction)
         {
             _direction = direction;
         }
 
+        /// <inheritdoc />
         public void ExecuteCommand(Character character)
         {
             character.MoveTo(_direction);
