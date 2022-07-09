2cb4cb6a0a715a4934b2153584be720c4fbb288e
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-07-09 22:43:44 +0200

    Add documentation

--- Francesco-Carlucci/Level.cs
+++ Francesco-Carlucci/Level.cs
@@ -1,19 +1,29 @@
-﻿
-using LorenzoTodisco;
+﻿using LorenzoTodisco;
 
 namespace Francesco_Carlucci
 {
+    /// <summary>
+    /// CLass that models a level.
+    /// </summary>
     public class Level
     {
         public List<AbstractItem> Items { get; }
         public Position DoorPosition { get; set; }
 
+        /// <summary>
+        /// constructor for a level.
+        /// </summary>
         public Level()
         {
             Items = new List<AbstractItem>();
             DoorPosition = new Position(0, 0);
         }
 
+        /// <summary>
+        /// Add items to the level
+        /// </summary>
+        /// <param name="itemsToAdd">items to add</param>
+        /// <returns></returns>
         public Level AddItems(params AbstractItem[] itemsToAdd)
         {
             foreach (AbstractItem i in itemsToAdd)
