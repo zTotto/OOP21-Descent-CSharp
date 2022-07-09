d7cdedf831ce927c25faa92174a42920e590a04a
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2022-07-09 21:42:51 +0200

    Add test class

---
+++ Francesco-Carlucci/Mob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LorenzoTodisco;
+
+namespace Francesco_Carlucci
+{
+    public class Mob : Character
+    {
+        public Mob(Weapon startingWeapon, int maxHp, int speed) : base(startingWeapon, maxHp, speed)
+        {
+        }
+    }
+}
