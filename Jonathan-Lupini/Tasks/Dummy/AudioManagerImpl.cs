7aa59b34bbde5fff8bc7318fca9bca0d36382d1d
Jonathan <95381542+JonathanLupiniUnibo@users.noreply.github.com>
2022-07-09 22:42:19 +0200

    changed AudioManagerImpl to use methods instead of accessing field

--- Jonathan-Lupini/Tasks/Dummy/AudioManagerImpl.cs
+++ Jonathan-Lupini/Tasks/Dummy/AudioManagerImpl.cs
@@ -8,43 +8,43 @@ namespace Jonathan_Lupini.Tasks.Dummy
         private readonly Dictionary<string, Music> _songs = new Dictionary<string, Music>();
         public void StopMusic(string path)
         {
-            if (IsAlreadyUsed(path)) _songs[path].Stop();
+            if (IsAlreadyUsed(path)) GetSong(path).Stop();
         }
 
         public void PauseMusic(string path)
         {
-            if (IsAlreadyUsed(path)) _songs[path].Pause();
+            if (IsAlreadyUsed(path)) GetSong(path).Pause();
         }
 
         public void PlayMusic(string path, bool looping, float volume)
         {
-            if (IsAlreadyUsed(path) && _songs[path].IsPlaying())
+            if (IsAlreadyUsed(path) && GetSong(path).IsPlaying())
             {
-                _songs[path].SetLooping(looping);
-                _songs[path].SetVolume(volume);
-                _songs[path].Play();
+                GetSong(path).SetLooping(looping);
+                GetSong(path).SetVolume(volume);
+                GetSong(path).Play();
             }
             else if (!IsAlreadyUsed(path))
             {
                 Music music = GdxAudio.NewMusic(GdxFiles.Internal(path));
                 _songs.Add(path, music);
-                _songs[path].SetLooping(looping);
-                _songs[path].SetVolume(volume);
-                _songs[path].Play();
+                GetSong(path).SetLooping(looping);
+                GetSong(path).SetVolume(volume);
+                GetSong(path).Play();
             }
         }
 
         public void DisposeMusic(string path)
         {
-            if (IsAlreadyUsed(path)) _songs[path].dispose();
+            if (IsAlreadyUsed(path)) GetSong(path).dispose();
         }
 
         public void ModifyMusic(string path, bool looping, float volume)
         {
             if (IsAlreadyUsed(path))
             {
-                _songs[path].SetLooping(looping);
-                _songs[path].SetVolume(volume);
+                GetSong(path).SetLooping(looping);
+                GetSong(path).SetVolume(volume);
             }
         }
 
@@ -52,21 +52,21 @@ namespace Jonathan_Lupini.Tasks.Dummy
         {
             if (IsAlreadyUsed(path))
             {
-                _soundEffects[path].SetVolume(0, volume);
-                _soundEffects[path].Play();
+                GetSound(path).SetVolume(0, volume);
+                GetSound(path).Play();
             }
             else
             {
                 Sound sound = GdxAudio.NewSound(GdxFiles.Internal(path));
                 sound.SetVolume(0, volume);
                 _soundEffects.Add(path, sound);
-                _soundEffects[path].Play();
+                GetSound(path).Play();
             }
         }
 
         public void ChangeSoundVolume(string path, float volume)
         {
-            if (_soundEffects.ContainsKey(path)) _soundEffects[path].SetVolume(0, volume);
+            if (_soundEffects.ContainsKey(path)) GetSound(path).SetVolume(0, volume);
         }
 
         bool IsAlreadyUsed(string path)
